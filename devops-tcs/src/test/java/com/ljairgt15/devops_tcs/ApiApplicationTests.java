9484f7539cb162970c4b20dfef972ac18513b246
JAIR GÓMEZ <jair.gomez@improve-it.com.ec>
2024-09-20 09:51:56 -0500

    feat: initiralizer

---
+++ devops-tcs/src/test/java/com/ljairgt15/devops_tcs/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljairgt15.devops_tcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
