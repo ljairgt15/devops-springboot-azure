d947ce44efc245af4f78236064f32a53e1b7f1fe
JAIR GÓMEZ <jair.gomez@improve-it.com.ec>
2025-01-31 16:18:20 -0500

    feat: equilibre secret in JWT wirh Azure API Management, el cual espera que la clave esté en Base64 cuando se usa <issuer-signing-keys>.       La razón es que APIM convierte la clave desde Base64 a bytes automáticamente antes de verificar la firma del JWT.
          Por eso, en APIM debías pasar la clave en Base64, pero en Java la clave estaba en hexadecimal, lo que causaba la discrepancia.

--- devops-tcs/src/main/java/com/ljairgt15/devops_tcs/ApiApplication.java
+++ devops-tcs/src/main/java/com/ljairgt15/devops_tcs/ApiApplication.java
@@ -2,11 +2,16 @@ package com.ljairgt15.devops_tcs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Base64;
+
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+		/*Secret in hexadecimal*/
+		String secret="09c53c457e150766aacce02ca27ec8929797b4a3b1093d5bc6aabc0f4b4c307d";
+		System.out.println("clave"+ Base64.getEncoder().encodeToString(secret.getBytes()));
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
